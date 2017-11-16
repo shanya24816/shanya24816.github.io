7f951ff98e4d720e5ac7fbdc0c66c943d2ab2130
shanya24816 <shanya.williams24816@gmail.com>
2017-11-15 17:55:20 -0800

    Add files via upload

---
+++ DACAinteractive.js
@@ -0,0 +1,23 @@
+function myfunction(){
+	document.getElementById("button").innerHTML = "Some words to say";
+
+}
+
+
+
+function orangefunction(){
+	document.getElementById("button").innerHTML = "gray";
+	
+}
+
+
+
+function greenfunction(){
+	document.getElementById("button").innerHTML = "green";
+}
+
+
+function bluefunction(){
+	document.getElementById("button").innerHTML = "rcvhbjvcxdzfxngmchv,";
+	document.getElementById("button").style.backgroundColor = "green";
+}
