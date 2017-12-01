a82056d3a7a03dc852e1bad95b4d2697540e3e3a
Shanya Williams <shanya.williams24816@gmail.com>
2017-11-30 17:56:32 -0800

    Add files via upload

--- DACAinteractive.js
+++ DACAinteractive.js
@@ -13,12 +13,12 @@ function seventeenfunction(){
 
 
 function eightteenfunction(){
-	document.getElementById("button").innerHTML = "In 2018, the number of people who will lose DACA is about equal to the number of teachers employed in California.";
+	document.getElementById("button").innerHTML = "In 2018, nearly half the number of people who eat McDonalds in a week will have lost DACA by 2018.";
 	document.getElementById("subtitle").innerHTML = "2018"
 }
 
 
 function nineteenfunction(){
-	document.getElementById("button").innerHTML = "In 2019, the number of people who will have lost DACA is about the number of Apple computers sold in a month.";
+	document.getElementById("button").innerHTML = "In 2019, the amount of people who will lose DACA will be nine times the number of people killed in a motor vehicle crash.";
 	document.getElementById("subtitle").innerHTML = "2019"
 }
